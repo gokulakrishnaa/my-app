0ed6bdf99dab3bae7b40c6dbd9dd8e9e2d0bbf7d
gokulakrishnaa <gokulakrishnaapsivam@gmail.com>
2021-11-04 18:29:00 +0530

    movie list with counter

--- src/App.js
+++ src/App.js
@@ -1,31 +1,34 @@
+import { useState } from "react";
 import "./App.css";
 
-// function App() {
-//   const details = [
-//     {
-//       name: "gokul",
-//       image:
-//         "https://hs-marketing.imgix.net/images/blog/steve-jobs.jpg?auto=compress&fit=scale&w=1600&dpr=1",
-//     },
-//     {
-//       name: "steve",
-//       image:
-//         "https://hs-marketing.imgix.net/images/blog/steve-jobs.jpg?auto=compress&fit=scale&w=1600&dpr=1",
-//     },
-//     {
-//       name: "elon",
-//       image:
-//         "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTc5OTk2ODUyMTMxNzM0ODcy/gettyimages-1229892983-square.jpg",
-//     },
-//   ];
-//   return (
-//     <div className="App">
-//       {details.map((nm) => (
-//         <Msg name={nm.name} image={nm.image} />
-//       ))}
-//     </div>
-//   );
-// }
+export default function App() {
+  // const details = [
+  //   {
+  //     name: "gokul",
+  //     image:
+  //       "https://hs-marketing.imgix.net/images/blog/steve-jobs.jpg?auto=compress&fit=scale&w=1600&dpr=1",
+  //   },
+  //   {
+  //     name: "steve",
+  //     image:
+  //       "https://hs-marketing.imgix.net/images/blog/steve-jobs.jpg?auto=compress&fit=scale&w=1600&dpr=1",
+  //   },
+  //   {
+  //     name: "elon",
+  //     image:
+  //       "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTc5OTk2ODUyMTMxNzM0ODcy/gettyimages-1229892983-square.jpg",
+  //   },
+  // ];
+  return (
+    <div className="App">
+      {/* {details.map((nm) => (
+        <Msg name={nm.name} image={nm.image} />
+      ))} */}
+
+      <Movie />
+    </div>
+  );
+}
 
 // function Msg({ name, image }) {
 //   return (
@@ -36,51 +39,66 @@ import "./App.css";
 //   );
 // }
 
-export default function Movie() {
+function Movie() {
   const movies = [
     {
       name: "Dark Knight Rises",
       poster: "https://m.media-amazon.com/images/I/51hcfZvo7FL.jpg",
-      rating: "8.4/10",
+      rating: "8.4",
       summary:
         "Bane, an imposing terrorist, attacks Gotham City and disrupts its eight-year-long period of peace. This forces Bruce Wayne to come out of hiding and don the cape and cowl of Batman again.",
     },
     {
       name: "Shawshank Redemption",
       poster: "https://m.media-amazon.com/images/I/71715eBi1sL._AC_SY741_.jpg",
-      rating: "9.3/10",
+      rating: "9.3",
       summary:
         "Andy Dufresne, a successful banker, is arrested for the murders of his wife and her lover, and is sentenced to life imprisonment at the Shawshank prison. He becomes the most unconventional prisoner.",
     },
     {
       name: "Nayagan",
       poster: "https://pbs.twimg.com/media/Cjxpt7aVEAAOfts.jpg",
-      rating: "8.7/10",
+      rating: "8.7",
       summary:
         "Velu Nayakan, who witnesses the brutal murder of his father, kills a corrupt policeman and escapes to Mumbai, only to become a gangster.",
     },
     {
-      name: "Thalapathi",
-      poster:
-        "https://images-na.ssl-images-amazon.com/images/I/616tUbWJV1L._RI_.jpg",
-      rating: "8.5/10",
-      summary:
-        "Surya, a courageous man who was raised in the slums, fights for the rights of the poor. However, he becomes friends with Deva who hires him as his commander and changes his life completely.",
-    },
-    {
       name: "Departed",
       poster: "https://upload.wikimedia.org/wikipedia/en/5/50/Departed234.jpg",
-      rating: "8.5/10",
+      rating: "8.5",
       summary:
         "An undercover agent and a spy constantly try to counter-attack each other in order to save themselves from being exposed in front of the authorities. Meanwhile, both try to infiltrate an Irish gang.",
     },
     {
       name: "HeyRam",
       poster:
-        "https://images-na.ssl-images-amazon.com/images/S/pv-target-images/d72222d1c74b369a8a72f52c658710a98e854c81f1d439a0e6663e9cc65c6128._UY500_UX667_RI_V_TTW_.png",
-      rating: "8/10",
+        "https://images-na.ssl-images-amazon.com/images/I/71corEX9jOL._RI_.jpg",
+      rating: "8",
       summary:
         "This fictional Indian drama tells the story of Saketh Ram (Kamal Hassan), a disillusioned man who tries to kill Mohandas Gandhi (Naseeruddin Shah). After sectarian violence claims the life of his wife, Aparna (Rani Mukharjee), Saketh becomes a Hindu extremist, and, although Muslims are responsible for his wife's death, he begins to blame Gandhi and his tolerant approach to religious differences.",
+    },
+    {
+      name: "Kannathil Muthamittal",
+      poster:
+        "https://c.saavncdn.com/024/Kannathil-Muthamittal-Tamil-2002-500x500.jpg",
+      rating: "8.4",
+      summary:
+        "At the age of nine, Amudha learns that she is an adopted child. Armed with a few bare facts, she desperately searches for her biological mother in war-ridden zones of Sri Lanka.",
+    },
+    {
+      name: "Jungle Book",
+      poster:
+        "https://lumiere-a.akamaihd.net/v1/images/p_thejunglebook2016_19751_6b8cfcec.jpeg",
+      rating: "7.5",
+      summary:
+        "Mowgli is a boy brought up in the jungle by a pack of wolves. When Shere Khan, a tiger, threatens to kill him, a panther and a bear help him escape his clutches.",
+    },
+    {
+      name: "Inception",
+      poster: "https://flxt.tmsimg.com/assets/p7825626_p_v10_af.jpg",
+      rating: "8.8",
+      summary:
+        "Cobb steals information from his targets by entering their dreams. Saito offers to wipe clean Cobb's criminal history as payment for performing an inception on his sick competitor's son.",
     },
   ];
   return (
@@ -98,18 +116,51 @@ export default function Movie() {
 }
 
 function Display({ name, poster, rating, summary }) {
+  const [show, setShow] = useState(true);
+  const styles = { color: rating > 8 ? "green" : "blue", fontWeight: "bold" };
+  const summaryStyles = { display: show ? "block" : "none" };
   return (
     <div className="display-movie">
-      <h1 className="movie-name">{name}</h1>
       <img className="movie-poster" src={poster} alt={name} />
-      <p className="movie-rating">
-        <b>Rating : </b>
-        {rating}
+      <div className="movie-specs">
+        <h3 className="movie-name">{name}</h3>
+        <p className="movie-rating" style={styles}>
+          ⭐ {rating}
+        </p>
+      </div>
+      <button onClick={() => setShow(!show)} className="description">
+        {show ? "Hide" : "Show"} Description
+      </button>
+      <p className="movie-summary" style={summaryStyles}>
+        Summary : {summary}
       </p>
-      <p className="movie-summary">
-        <b>Summary : </b>
-        {summary}
-      </p>
+      {/* {show ? <p className="movie-summary">Summary : {summary}</p> : ""} */}
+      <Counter />
+    </div>
+  );
+}
+
+function Counter() {
+  const [like, setlike] = useState(0);
+  const [dislike, setdislike] = useState(0);
+  return (
+    <div className="counter-container">
+      <button
+        className="likes-dislikes"
+        onClick={() => {
+          setlike(like + 1);
+        }}
+      >
+        👍 {like}
+      </button>
+      <button
+        className="likes-dislikes"
+        onClick={() => {
+          setdislike(dislike + 1);
+        }}
+      >
+        👎 {dislike}
+      </button>
     </div>
   );
 }
