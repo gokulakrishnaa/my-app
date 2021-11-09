0eeeca62342051b9ae64c700d5fa6f52bb569f0a
gokulakrishnaa <gokulakrishnaapsivam@gmail.com>
2021-11-09 06:30:18 +0530

    Color Added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
+import { useState } from "react";
 import "./App.css";
 import { Movie } from "./Movie";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
 
 export default function App() {
   // const details = [
@@ -25,7 +28,46 @@ export default function App() {
         <Msg name={nm.name} image={nm.image} />
       ))} */}
 
-      <Movie />
+      {/* <Movie /> */}
+      <AddColor />
     </div>
   );
+}
+
+function AddColor() {
+  const [color, setColor] = useState("red");
+  const styles = { backgroundColor: color };
+  const [colors, setColors] = useState(["teal", "orange", "lavender"]);
+  return (
+    <div>
+      <div className="color-details">
+        <TextField
+          value={color}
+          onChange={(event) => setColor(event.target.value)}
+          style={styles}
+          label="Enter a color"
+          variant="standard"
+        />
+        <Button
+          onClick={() => setColors([...colors, color])}
+          variant="contained"
+        >
+          Add Button
+        </Button>
+      </div>
+      {colors.map((clr) => (
+        <ColorBox color={clr} />
+      ))}
+    </div>
+  );
+}
+
+function ColorBox({ color }) {
+  const styles = {
+    backgroundColor: color,
+    height: "20px",
+    width: "200px",
+    marginTop: "10px",
+  };
+  return <div style={styles}></div>;
 }
